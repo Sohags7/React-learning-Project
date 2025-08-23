b02371a1866ccc25bd88cbf2fb16411d85c167ef
Sohags7 <sohag5050009@gmail.com>
2025-08-23 20:58:46 +0600

    Simple incre decrement with history truncate.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Counter from './Componants/Counter'
+
+function App() {
+  return (
+   <>
+   <div className='text-center '>
+    <Counter />
+   </div>
+   
+   </>
+  )
+}
+
+export default App
