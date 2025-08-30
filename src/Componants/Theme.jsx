a57633f06353b2a1127c936d0a93c7091f110a25
SohagAhmed-23 <sohag.ahmed@brainstation-23.com>
2025-08-31 02:04:44 +0600

    storage add

--- src/Componants/Theme.jsx
+++ src/Componants/Theme.jsx
@@ -1,19 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ThemeContext } from "../App";
 
+ 
 function Theme() {
   const { theme, setTheme } = useContext(ThemeContext);
 
+  useEffect(() => {
+     const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) setTheme(storedTheme);
+
+  },[setTheme])
+ 
+  const switchTheme = () => {
+    const newTheme = theme === "black" ? "white" : "black";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
   return (
     <div className="text-center">
       <h1 className="text-3xl mb-4" style={{ color: theme === "black" ? "white" : "black" }}>
         Current Theme: {theme}
       </h1>
       <button
-        onClick={() => setTheme(theme === "black" ? "white" : "black")}
-        className="px-4 py-2 rounded-lg border-2 "
+        onClick={() => switchTheme() }
+        className="px-4 py-2 cursor-pointer rounded-lg border-2 "
         style={{ borderColor: theme === "black" ? "white" : "black" }}
-      > {theme== "black" ? (<span className="text-white">Toggle</span>):(<span className="black">Toggle</span>)}
+      > {theme== "black" ? (<span className="text-white">Toggle</span>):
+        (
+        <span className="textblack">Toggle</span>)}
       </button>
     </div>
   );
